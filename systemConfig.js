e4c76ac4d2faa1c8f9ed46b0d51637d86cf1f38d
Jaffar <37204341+jaffarChatApp@users.noreply.github.com>
2018-03-12 11:34:31 +0530

    URL changed

--- systemConfig.js
+++ systemConfig.js
@@ -1,18 +1,18 @@
 /*Prod URl Configuration*/
-/*ChatApp.constant('CONFIG', {
+ChatApp.constant('CONFIG', {
     'name': 'Chat App',
     'version': '1.0',
     'rootUrl': "https://stg.paycheckalpha.com:1880/chatApp/", // Production Server
-    'chatUrl': "ws://stg.paycheckalpha.com:1880/ws/getChats", // Production WS
-});*/
+    'chatUrl': "wss://stg.paycheckalpha.com:1880/ws/getChats", // Production WS
+});
 
 /*Remote URl Configuration*/
-ChatApp.constant('CONFIG', {
+/*ChatApp.constant('CONFIG', {
     'name': 'Chat App',
     'version': '1.0',
     'rootUrl': "http://192.168.1.98:1997/chatApp/", // Production Server
     'chatUrl': "ws://192.168.1.98:1997/ws/getChats", // Production WS
-});
+});*/
 
 /*Dev URl Configuration*/
 /*
