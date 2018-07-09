699166d0a4f54848a66339af4deab9e0fb4efd1d
jaffar <jaffird2310@gmail.com>
2018-07-09 13:51:42 +0530

    Disable subscribe btn

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,8 @@ var OneSignal = window.OneSignal || [];
 OneSignal.push(function () {
     OneSignal.init({
         appId: "fe9010f6-0bb3-464a-9e55-6aab21b2f944",
+        notifyButton: {
+            enable: false,
+        }
     });
 });
